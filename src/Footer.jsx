1c00fb92d960ff03b86e040ac7ac8cb792e5d04a
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-11-09 16:12:17 -0300

    Employees, Header and Footer components

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
+  let today = new Date();
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='container'>
+      <div className='row justify-content-center mb-3 mt-3'>
+        <div className='col-8'>
+          <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+        </div>
+      </div>
     </footer>
   );
 };
